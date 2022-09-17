e5acbdc4df1347323e4de831aa65a6828f425250
Yerjan <yerjan@proton.me>
2022-09-17 23:00:58 +0300

    ex22 done

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-let password = "Hell0";
-let passLength = password.length;
-console.log(
-  4 <= passLength && passLength <= 6
-    ? " Password length is correct"
-    : "Password not valid"
-);
+let primitive = true;
+
+switch (typeof primitive) {
+  case "number":
+    console.log(`data type of '${primitive}' is number`);
+    break;
+  case "string":
+    console.log(`data type of '${primitive}' is string`);
+    break;
+  case "boolean":
+    console.log(`data type of '${primitive}' is boolean`);
+    break;
+}
