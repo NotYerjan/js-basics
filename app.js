79030af98012414c1004b9d1f4b1f9eed584e1b3
Yerjan <yerjan@proton.me>
2022-09-17 23:09:41 +0300

    ex24 done

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-let roomType = "Basic Room";
-
-switch (roomType.toLowerCase()) {
-  case "basic room":
-    console.log(`50€`);
-    break;
-  case "junior suite room":
-    console.log(`80€`);
-    break;
-  case "master suite":
-    console.log(`100€`);
-    break;
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
 }
