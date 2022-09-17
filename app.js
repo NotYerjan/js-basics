b6fb5f78776174720f1081c16eb21803b9b76b62
Yerjan <yerjan@proton.me>
2022-09-17 20:16:13 +0300

    ex10 done

--- app.js
+++ app.js
@@ -1,26 +1,7 @@
-const name = "Mario";
-const lastname = "Rossi";
-const age = 27;
-const isGraduated = false;
-const average = 27;
-const examsCompleted = 10;
 const firstYearCompleted = true;
 const yearsCompleted = 1;
 
-// name == lastname; // false
-// age <= average; // true
-// firstYearCompleted == lastname; // true
-// yearsCompleted == firstYearCompleted; // true
-// firstYearCompleted === yearsCompleted; //false
-// examsCompleted < age; //false
-// isGraduated > yearsCompleted; // undefined
-
-console.log(
-  name == lastname,
-  age <= average,
-  firstYearCompleted == lastname,
-  yearsCompleted == firstYearCompleted,
-  firstYearCompleted === yearsCompleted,
-  examsCompleted < age,
-  isGraduated > yearsCompleted
-);
+yearsCompleted == firstYearCompleted;
+// we check if they are equivalent in the different data type
+yearsCompleted === firstYearCompleted;
+// we check if they are equivalent in same same data type
