72fbc8be243b5bd238d4c04f4c75547495e7078f
Yerjan <yerjan@proton.me>
2022-09-25 00:05:47 +0300

    ex53 done

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-const students = [
-  { id: 1, name: "Luca", surname: "Rossi", age: 20 },
-  { id: 2, name: "Mario", surname: "Bianchi", age: 24 },
-  { id: 3, name: "Giovanni", surname: "Brambilla", age: 30 },
+function adultFilter(persons) {
+  return persons.filter((person) => person.age >= 18);
+}
+
+const persons = [
+  { name: "Paul", age: 16 },
+  { name: "George", age: 17 },
+  { name: "Lucas", age: 21 },
+  { name: "Marco", age: 32 },
+  { name: "Peter", age: 18 },
+  { name: "Carl", age: 13 },
+  { name: "Simon", age: 24 },
+  { name: "Mark", age: 15 },
+  { name: "Sandra", age: 34 },
+  { name: "Alice", age: 28 },
 ];
 
-students.pop();
-
-let newStudent = { id: 3, name: "Francesco", surname: "Verdi", age: 41 };
-students.push(newStudent);
-
-console.log(students);
+const adults = adultFilter(persons);
+console.log(persons);
+console.log(adults);
