3b3ceb767ee3164ce2f430902b01468a5aefac71
Yerjan <yerjan@proton.me>
2022-09-24 23:12:16 +0300

    ex47 done

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-const student = {
-  id: 1,
-  school: "Liceo",
-  year: 3,
-  personalData: {
-    name: "Yerjan",
-    surname: "Turarov",
-    age: 24,
-  },
-};
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(
+  numbers[0],
+  numbers[numbers.length - 1],
+  numbers.length,
+  numbers[20] // 21st element of the array is empty that's why the output will be undefined
+);
