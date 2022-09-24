f062c2a44e75711fc610e19dff6aac15a2ad8b00
Yerjan <yerjan@proton.me>
2022-09-24 23:01:34 +0300

    ex46 done

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-function Smartphone(brand, name, price) {
-  this.brand = brand;
-  this.name = name;
-  this.price = price;
-}
-
-const iphone = new Smartphone("Apple", "Iphone 13", 1300);
-const android = new Smartphone("Samsung", "Galaxy S22", 1200);
-
-console.log(iphone, android);
+const student = {
+  id: 1,
+  school: "Liceo",
+  year: 3,
+  personalData: {
+    name: "Yerjan",
+    surname: "Turarov",
+    age: 24,
+  },
+};
