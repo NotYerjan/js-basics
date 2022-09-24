9011187fb7a0df2e941ef8853bb4ea1a9f7bd79a
Yerjan <yerjan@proton.me>
2022-09-24 23:40:18 +0300

    ex50 done

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-const students = [
-  { id: 1, name: "Luca", surname: "Rossi", age: 20 },
-  { id: 2, name: "Mario", surname: "Bianchi", age: 24 },
-  { id: 3, name: "Giovanni", surname: "Brambilla", age: 30 },
-];
-students.map((student) => {
-  console.log(student);
-});
+const names = ["Luca", "Marco", "Vittorio", "Giovanni"];
+names.pop();
+names.push("Pippo");
+names.unshift("Giovanni");
+
+console.log(names);
