785c731ce83af2d2f8713892e68a96af8ee5c1b1
Yerjan <yerjan@proton.me>
2022-09-24 21:39:21 +0300

    ex36 done

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
+let sport = " Football";
 function canPlay() {
-  let sport = " Football";
-
   if (true) {
     let personName = "Cosimo";
     console.log(personName + sport);
   }
 }
-
-// personName can be called only in the if satetement
 
 canPlay();
