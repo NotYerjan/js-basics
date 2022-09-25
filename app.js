749694867c40bc85f93e9a48a2359e9f8333722a
Yerzhan Turarov <105886184+YerjanDesign@users.noreply.github.com>
2022-09-25 13:35:33 +0300

    update

--- app.js
+++ app.js
@@ -4,5 +4,4 @@ console.log(
   numbers[0],
   numbers[numbers.length - 1],
   numbers.length,
-  numbers[20] // 21st element of the array is empty that's why the output will be undefined
-);
+  numbers[20] // the last item of the array is stored in index 9 and array will occupy only 10 cells that's why other cells will be undefined ;
