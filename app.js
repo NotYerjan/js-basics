d824c2dc556c1a65fcd5f575564b6449931c998b
Yerzhan Turarov <105886184+YerjanDesign@users.noreply.github.com>
2022-09-25 13:36:15 +0300

    new update

--- app.js
+++ app.js
@@ -5,3 +5,4 @@ console.log(
   numbers[numbers.length - 1],
   numbers.length,
   numbers[20] // the last item of the array is stored in index 9 and array will occupy only 10 cells that's why other cells will be undefined ;
+)
